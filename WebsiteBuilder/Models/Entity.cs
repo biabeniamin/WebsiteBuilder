d08d21a9848f742fc719ac69b5266037cab2bf57
bb <biabeniamin@gmail.com>
2018-12-25 19:41:21 +0200

    models for entity and response

---
+++ WebsiteBuilder/Models/Entity.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteBuilder.Models
+{
+    public class Entity
+    {
+        [JsonProperty("entity")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("startIndex")]
+        public int StartIndex { get; set; }
+
+        [JsonProperty("endIndex")]
+        public int EndIndex { get; set; }
+
+        [JsonProperty("score")]
+        public double Score { get; set; }
+    }
+}
---
+++ WebsiteBuilder/Models/Response.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteBuilder.Models
+{
+    public class Response
+    {
+        [JsonProperty("query")]
+        public string Query { get; set; }
+
+        [JsonProperty("topScoringIntent")]
+        public TopScoringIntent TopScoringIntent { get; set; }
+
+        [JsonProperty("intents")]
+        public IList<Intent> Intents { get; set; }
+
+        [JsonProperty("entities")]
+        public IList<Entity> Entities { get; set; }
+    }
+}
