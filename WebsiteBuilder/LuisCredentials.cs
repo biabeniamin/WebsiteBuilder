f07436360f68d16afd4d76d6d7d19f8eeb45a1bc
bb <biabeniamin@gmail.com>
2018-12-25 17:51:35 +0200

    Api key file added

---
+++ WebsiteBuilder/LuisCredentials.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteBuilder
+{
+    public static class LuisCredentials
+    {
+        public static string AppId
+        {
+            get
+            {
+                return "APP_ID";
+            }
+        }
+
+        public static string EndpointKey
+        {
+            get
+            {
+                return "ENDPOINT_KEY";
+            }
+        }
+    }
+}
