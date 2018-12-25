c9b15ba98076ab7dcc5868c9b786fdf0bbb268c5
bb <biabeniamin@gmail.com>
2018-12-25 19:53:27 +0200

    luis class implemented

---
+++ WebsiteBuilder/Luis.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using WebsiteBuilder.Models;
+
+namespace WebsiteBuilder
+{
+    public class Luis
+    {
+        async Task<Response> SendMessage(string question)
+        {
+            Response response;
+            HttpClient client;
+            HttpResponseMessage responseHttp;
+            string endpointUri;
+            string responseJson;
+
+            response = null;
+            client = new HttpClient();
+            endpointUri = "";
+            responseJson = "";
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // The request header contains your subscription key
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", LuisCredentials.EndpointKey);
+
+            // The "q" parameter contains the utterance to send to LUIS
+            queryString["q"] = question;
+
+            // These optional request parameters are set to their default values
+            queryString["timezoneOffset"] = "0";
+            queryString["verbose"] = "false";
+            queryString["spellCheck"] = "false";
+            queryString["staging"] = "false";
+
+            endpointUri = $"https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/{LuisCredentials.AppId}?{queryString}";
+            responseHttp = await client.GetAsync(endpointUri);
+
+            responseJson = await responseHttp.Content.ReadAsStringAsync();
+
+            response = JsonConvert.DeserializeObject<Response>(responseJson.ToString());
+
+            return response;
+        }
+    }
+}
