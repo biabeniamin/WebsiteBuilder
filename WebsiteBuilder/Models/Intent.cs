4d2a0dd6932e3d7134e06816ab39348f64e40536
bb <biabeniamin@gmail.com>
2018-12-25 19:39:56 +0200

    intent and top scoring model

---
+++ WebsiteBuilder/Models/Intent.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteBuilder.Models
+{
+    public class Intent
+    {
+        [JsonProperty("intent")]
+        public string Name { get; set; }
+
+        [JsonProperty("score")]
+        public double Score { get; set; }
+    }
+}
---
+++ WebsiteBuilder/Models/TopScoringIntent.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteBuilder.Models
+{
+    public class TopScoringIntent
+    {
+
+        [JsonProperty("intent")]
+        public string Intent { get; set; }
+
+        [JsonProperty("score")]
+        public double Score { get; set; }
+    }
+}
