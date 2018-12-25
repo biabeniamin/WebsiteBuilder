3e118cb3744c75a8807e62e6279d60cdaaa87d07
bb <biabeniamin@gmail.com>
2018-12-25 18:01:26 +0200

    small api example to work with luis

--- WebsiteBuilder/MainWindow.xaml.cs
+++ WebsiteBuilder/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +25,40 @@ namespace WebsiteBuilder
         public MainWindow()
         {
             InitializeComponent();
+
+            MakeRequest();
+
         }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // This app ID is for a public sample app that recognizes requests to turn on and turn off lights
+            var luisAppId = LuisCredentials.AppId;
+            var endpointKey = LuisCredentials.EndpointKey;
+
+            // The request header contains your subscription key
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", endpointKey);
+
+            // The "q" parameter contains the utterance to send to LUIS
+            queryString["q"] = "i want to build a website";
+
+            // These optional request parameters are set to their default values
+            queryString["timezoneOffset"] = "0";
+            queryString["verbose"] = "false";
+            queryString["spellCheck"] = "false";
+            queryString["staging"] = "false";
+
+            var endpointUri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/" + luisAppId + "?" + queryString;
+            var response = await client.GetAsync(endpointUri);
+
+            var strResponseContent = await response.Content.ReadAsStringAsync();
+
+            // Display the JSON result from LUIS
+            Console.WriteLine(strResponseContent.ToString());
+        }
+
     }
 }
