ddabee4f6226663a693c48ebfc99ebb75fc094c5
bb <biabeniamin@gmail.com>
2018-12-25 22:59:53 +0200

    background added in website model

--- WebsiteBuilder/Models/Website.cs
+++ WebsiteBuilder/Models/Website.cs
@@ -9,12 +9,31 @@ namespace WebsiteBuilder.Models
     public class Website
     {
         private string name;
+        private Background background;
+
+        public Background Background
+        {
+            get { return background; }
+            set { background = value; }
+        }
 
         public string Name
         {
             get { return name; }
             set { name = value; }
         }
+
+        public Website(string name,Background background)
+        {
+            this.name = name;
+            this.background = background;
+        }
+
+        public Website(string name)
+            :this(name, new Background(Color.White))
+        {
+
+        } 
 
     }
 }
