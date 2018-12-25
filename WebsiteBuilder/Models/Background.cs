a0b283a62d1f9eeb856306073a9877793c5788c7
bb <biabeniamin@gmail.com>
2018-12-25 22:54:42 +0200

    background model added

---
+++ WebsiteBuilder/Models/Background.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteBuilder.Models
+{
+    public class Background
+    {
+        private Color backgroundColor;
+        private Image image;
+
+        public Image Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+        public Color BackgroundColor
+        {
+            get { return backgroundColor; }
+            set { backgroundColor = value; }
+        }
+
+        public bool HasImageAsBackground
+        {
+            get
+            {
+                return !(image.Path == "no_image"
+                    || !String.IsNullOrEmpty(image.Path));
+            }
+        }
+
+        public Background(Color color)
+        {
+            backgroundColor = color;
+            image = new Image("no_image");
+        }
+
+        public Background(Image image)
+        {
+            backgroundColor = Color.White;
+            this.image = image;
+        }
+    }
+}
