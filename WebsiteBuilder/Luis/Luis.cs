61a3bafcd6cb64369cfa8e10749530bf78d9f76c
bb <biabeniamin@gmail.com>
2018-12-26 12:02:08 +0200

    namespace modified for new project

--- WebsiteBuilder/Luis/Luis.cs
+++ WebsiteBuilder/Luis/Luis.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using System.Web;
 using WebsiteBuilder.Models;
 
-namespace WebsiteBuilder
+namespace Luis
 {
     public class Luis
     {
--- WebsiteBuilder/Luis/Models/TopScoringIntent.cs
+++ WebsiteBuilder/Luis/Models/TopScoringIntent.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WebsiteBuilder.Models
+namespace Luis.Models
 {
     public class TopScoringIntent
     {
