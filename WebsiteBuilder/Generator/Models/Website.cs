48a9562041fd3897cf138021d42c15c543140afd
Beniamin Bia <biabeniamin@gmail.com>
2019-01-12 20:58:44 +0200

    models moved to generator project

--- WebsiteBuilder/Generator/Models/Website.cs
+++ WebsiteBuilder/Generator/Models/Website.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebsiteBuilder.Enums;
+using Generator.Enums;
 
-namespace WebsiteBuilder.Models
+namespace Generator.Models
 {
     public class Website
     {
