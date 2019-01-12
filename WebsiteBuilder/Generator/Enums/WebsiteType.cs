578feae9aebfb31e0a80ac7ce76e974d5da820ba
Beniamin Bia <biabeniamin@gmail.com>
2019-01-12 20:58:27 +0200

    enums moved to generator

---
+++ WebsiteBuilder/Generator/Enums/WebsiteType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator.Enums
+{
+    public enum WebsiteType
+    {
+        Blog,
+        Business,
+        ECommerce,
+        Organisation,
+        Portofolio,
+        Community
+    }
+}
