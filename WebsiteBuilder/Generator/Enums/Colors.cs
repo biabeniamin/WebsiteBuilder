437e1f958dbd8d278360a50608e80d41e01fb8a8
Beniamin Bia <biabeniamin@gmail.com>
2019-01-12 21:28:17 +0200

    color enum added

---
+++ WebsiteBuilder/Generator/Enums/Colors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator.Enums
+{
+    public enum Colors
+    {
+        White = 0x000000,
+        Red = 0xFF0000,
+        Green = 0x00FF00,
+        Blue = 0x0000FF,
+    }
+}
