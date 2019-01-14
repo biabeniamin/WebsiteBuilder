7a031cc45ba4fa66653d776b3db8cb81516f2a2c
Beniamin Bia <biabeniamin@gmail.com>
2019-01-14 18:37:17 +0200

    response class implemented

--- WebsiteBuilder/Generator/Models/Response.cs
+++ WebsiteBuilder/Generator/Models/Response.cs
@@ -8,5 +8,17 @@ namespace Generator.Models
 {
     public class Response
     {
+        private string textResponse;
+
+        public string TextResponse
+        {
+            get { return textResponse; }
+            set { textResponse = value; }
+        }
+
+        public Response(string textResponse)
+        {
+            this.textResponse = textResponse;
+        }
     }
 }
